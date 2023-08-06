f66fd21489557a60e8366155c77144eaf3ee5fff
Amadou <amadouamos@hotmail.com>
2023-08-05 19:03:31 -0600

    FInal Deploy With JS and CSS Implemented

--- scripts/booking.js
+++ scripts/booking.js
@@ -15,7 +15,7 @@ const half_day_button = document.getElementById("half");
 const full_day_button = document.getElementById("full");
 
 const remove_button = document.getElementById("clear-button");
-const calculated_cost = document.getElementById("calculated-cost");
+let calculated_cost = document.getElementById("calculated-cost");
 /********* colour change days of week *********/
 // when the day buttons are clicked, we will apply the "clicked" class to that element, and update any other relevant variables. Then, we can recalculate the total cost.
 // added challenge: don't update the dayCounter if the same day is clicked more than once. hint: .classList.contains() might be helpful here!
--- scripts/contact.js
+++ scripts/contact.js
@@ -1,4 +1,14 @@
 // when the "submit-button" is clicked, the contents of the contact-page are replaced with a single <p> element that reads "Thank you for your message" in size 24 font.
 
 // hint: you can change the style of an element by modifying the value of that element's .style.fontSize, or by updating its .classList.
+var main_content = document.getElementById("contact-page");
+const submitButton = document.getElementById("submit-button");
 
+function display_message() {
+    return "<p>Thank you for your message!</p>";
+}
+
+submitButton.addEventListener("click", function() {
+    main_content.innerHTML = display_message();
+    main_content.style.fontSize="24px"
+});
